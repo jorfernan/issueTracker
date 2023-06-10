55958a153a45c8c8fa9ec1d4992b9621a0569fb7
jorfernan <jorfernan1995@gmail.com>
2023-06-10 18:34:30 +0200

    eslint added

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,89 +1,105 @@
 const { MongoClient } = require("mongodb");
 
-require("dotenv").config();
+const path = require("path");
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_USER_PASSWD;
-const DB_NAME = process.env.DB_NAME;
-const DB_COLLECTION = process.env.DB_COLLECTION;
-const DB_CLUSTER=process.env.DB_CLUSTER;
-const DB_COLLECTION_COUNTERS=process.env.DB_COLLECTION_COUNTERS;
+const envPath = path.resolve(__dirname, "../../.env");
 
-async function initializeDB(){
+require("dotenv").config({ path: envPath });
 
-    const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/`;
+const {
+  DB_USER,
+  DB_USER_PASSWD,
+  DB_NAME,
+  DB_COLLECTION,
+  DB_CLUSTER,
+  DB_COLLECTION_COUNTERS,
+} = process.env;
 
-    const issuesDB = [
-        {
-            id: 1,
-            status: "New",
-            owner: "Ravan",
-            effort: 5,
-            created: new Date("2018-08-15"),
-            due: undefined,
-            title: "Error in console when clicking Add",
-          },
-          {
-            id: 2,
-            status: "Assigned",
-            owner: "Eddie",
-            effort: 14,
-            created: new Date("2018-08-16"),
-            due: new Date("2018-08-30"),
-            title: "Missing bottom border on panel",
-          },
-          {
-            id: 3,
-            status: "Fixed",
-            owner: "Jorge",
-            effort: 18,
-            created: new Date("2018-08-12"),
-            due: new Date("2018-08-22"),
-            title: "Database collection testing",
-          },
-    ];
+async function initializeDB() {
+  const url = `mongodb+srv://${DB_USER}:${DB_USER_PASSWD}@${DB_CLUSTER}/`;
 
-    console.log("\n--- Initializing Mongo DB ---\n");
-    const client = new MongoClient(url, { useNewUrlParser: true });
-  
-    try {
-        await client.connect();
-        
-        console.log("\n--- Connected to MongoDB ---");
-        const db = client.db(DB_NAME);
+  const issuesDB = [
+    {
+      id: 1,
+      status: "New",
+      owner: "Ravan",
+      effort: 5,
+      created: new Date("2018-08-15"),
+      due: undefined,
+      title: "Error in console when clicking Add",
+    },
+    {
+      id: 2,
+      status: "Assigned",
+      owner: "Eddie",
+      effort: 14,
+      created: new Date("2018-08-16"),
+      due: new Date("2018-08-30"),
+      title: "Missing bottom border on panel",
+    },
+    {
+      id: 3,
+      status: "Fixed",
+      owner: "Jorge",
+      effort: 18,
+      created: new Date("2018-08-12"),
+      due: new Date("2018-08-22"),
+      title: "Database collection testing",
+    },
+  ];
 
-        console.log(`\n--- Deleting all documents from "${DB_NAME}.${DB_COLLECTION}" ---`);
-        await db.collection(DB_COLLECTION).deleteMany({});
+  console.log("\n--- Initializing Mongo DB ---\n");
+  const client = new MongoClient(url, { useNewUrlParser: true });
 
-        console.log(`\n--- Inserting new documents to "${DB_NAME}.${DB_COLLECTION}" ---`);
-        await db.collection(DB_COLLECTION).insertMany(issuesDB);
-        const count = await db.collection(DB_COLLECTION).countDocuments();
-        console.log(`\nNumber of documents inserted: ${count}\n`);
+  try {
+    await client.connect();
 
-        console.log(`\n--- Creating indexes in "${DB_NAME}.${DB_COLLECTION}" ---\n`);
-        await db.collection(DB_COLLECTION).createIndex({ id: 1}, {unique: true });
-        await db.collection(DB_COLLECTION).createIndex({ status: 1});
-        await db.collection(DB_COLLECTION).createIndex({ owner: 1});
-        await db.collection(DB_COLLECTION).createIndex({ created: 1});
+    console.log("\n--- Connected to MongoDB ---");
+    const db = client.db(DB_NAME);
 
-        console.log(`\n--- Deleting ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" ---\n`);
-        await db.collection(DB_COLLECTION_COUNTERS).deleteOne({ _id: 'issues'});
+    console.log(
+      `\n--- Deleting all documents from "${DB_NAME}.${DB_COLLECTION}" ---`
+    );
+    await db.collection(DB_COLLECTION).deleteMany({});
 
-        console.log(`\n--- Creating ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" for "${DB_COLLECTION}" collection" ---\n`);
-        await db.collection(DB_COLLECTION_COUNTERS).insertOne({_id: 'issues', current: count});
+    console.log(
+      `\n--- Inserting new documents to "${DB_NAME}.${DB_COLLECTION}" ---`
+    );
+    await db.collection(DB_COLLECTION).insertMany(issuesDB);
+    const count = await db.collection(DB_COLLECTION).countDocuments();
+    console.log(`\nNumber of documents inserted: ${count}\n`);
 
-        const indexes = await db.collection(DB_COLLECTION).indexes();
-        console.log(`Generated indexes:\n`);
-        indexes.forEach(index => {
-            console.log(index);
-        });
+    console.log(
+      `\n--- Creating indexes in "${DB_NAME}.${DB_COLLECTION}" ---\n`
+    );
+    await db.collection(DB_COLLECTION).createIndex({ id: 1 }, { unique: true });
+    await db.collection(DB_COLLECTION).createIndex({ status: 1 });
+    await db.collection(DB_COLLECTION).createIndex({ owner: 1 });
+    await db.collection(DB_COLLECTION).createIndex({ created: 1 });
 
-    } catch (err) {
-      console.log(err);
-    } finally {
-      client.close();
-      console.log("\n--- Connection closed ---\n");
-    }
+    console.log(
+      `\n--- Deleting ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" ---\n`
+    );
+    await db.collection(DB_COLLECTION_COUNTERS).deleteOne({ _id: "issues" });
+
+    console.log(
+      `\n--- Creating ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" for "${DB_COLLECTION}" collection" ---\n`
+    );
+    await db
+      .collection(DB_COLLECTION_COUNTERS)
+      .insertOne({ _id: "issues", current: count });
+
+    const indexes = await db.collection(DB_COLLECTION).indexes();
+    console.log(`Generated indexes:\n`);
+    indexes.forEach((index) => {
+      console.log(index);
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.close();
+    console.log("\n--- Connection closed ---\n");
   }
+}
 
-  initializeDB();
+initializeDB();
