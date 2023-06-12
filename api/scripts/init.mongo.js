7052fe613504e0bc63e490bc9ff06955a994a4fc
jorfernan <jorfernan1995@gmail.com>
2023-06-12 09:52:13 +0200

    ESLINT errors and warnings solved by airbnb-base standard

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,10 +1,12 @@
-const { MongoClient } = require("mongodb");
+/* eslint no-restricted-globals: "off" */
 
-const path = require("path");
+const { MongoClient } = require('mongodb');
 
-const envPath = path.resolve(__dirname, "../../.env");
+const path = require('path');
 
-require("dotenv").config({ path: envPath });
+const envPath = path.resolve(__dirname, '../../.env');
+
+require('dotenv').config({ path: envPath });
 
 const {
   DB_USER,
@@ -21,76 +23,66 @@ async function initializeDB() {
   const issuesDB = [
     {
       id: 1,
-      status: "New",
-      owner: "Ravan",
+      status: 'New',
+      owner: 'Ravan',
       effort: 5,
-      created: new Date("2018-08-15"),
+      created: new Date('2018-08-15'),
       due: undefined,
-      title: "Error in console when clicking Add",
+      title: 'Error in console when clicking Add',
     },
     {
       id: 2,
-      status: "Assigned",
-      owner: "Eddie",
+      status: 'Assigned',
+      owner: 'Eddie',
       effort: 14,
-      created: new Date("2018-08-16"),
-      due: new Date("2018-08-30"),
-      title: "Missing bottom border on panel",
+      created: new Date('2018-08-16'),
+      due: new Date('2018-08-30'),
+      title: 'Missing bottom border on panel',
     },
     {
       id: 3,
-      status: "Fixed",
-      owner: "Jorge",
+      status: 'Fixed',
+      owner: 'Jorge',
       effort: 18,
-      created: new Date("2018-08-12"),
-      due: new Date("2018-08-22"),
-      title: "Database collection testing",
+      created: new Date('2018-08-12'),
+      due: new Date('2018-08-22'),
+      title: 'Database collection testing',
     },
   ];
 
-  console.log("\n--- Initializing Mongo DB ---\n");
+  console.log('\n--- Initializing Mongo DB ---\n');
   const client = new MongoClient(url, { useNewUrlParser: true });
 
   try {
     await client.connect();
 
-    console.log("\n--- Connected to MongoDB ---");
+    console.log('\n--- Connected to MongoDB ---');
     const db = client.db(DB_NAME);
 
-    console.log(
-      `\n--- Deleting all documents from "${DB_NAME}.${DB_COLLECTION}" ---`
-    );
+    console.log(`\n--- Deleting all documents from "${DB_NAME}.${DB_COLLECTION}" ---`);
     await db.collection(DB_COLLECTION).deleteMany({});
 
-    console.log(
-      `\n--- Inserting new documents to "${DB_NAME}.${DB_COLLECTION}" ---`
-    );
+    console.log(`\n--- Inserting new documents to "${DB_NAME}.${DB_COLLECTION}" ---`);
     await db.collection(DB_COLLECTION).insertMany(issuesDB);
     const count = await db.collection(DB_COLLECTION).countDocuments();
     console.log(`\nNumber of documents inserted: ${count}\n`);
 
-    console.log(
-      `\n--- Creating indexes in "${DB_NAME}.${DB_COLLECTION}" ---\n`
-    );
+    console.log(`\n--- Creating indexes in "${DB_NAME}.${DB_COLLECTION}" ---\n`);
     await db.collection(DB_COLLECTION).createIndex({ id: 1 }, { unique: true });
     await db.collection(DB_COLLECTION).createIndex({ status: 1 });
     await db.collection(DB_COLLECTION).createIndex({ owner: 1 });
     await db.collection(DB_COLLECTION).createIndex({ created: 1 });
 
-    console.log(
-      `\n--- Deleting ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" ---\n`
-    );
-    await db.collection(DB_COLLECTION_COUNTERS).deleteOne({ _id: "issues" });
+    console.log(`\n--- Deleting ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" ---\n`);
+    await db.collection(DB_COLLECTION_COUNTERS).deleteOne({ _id: 'issues' });
 
-    console.log(
-      `\n--- Creating ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" for "${DB_COLLECTION}" collection" ---\n`
-    );
+    console.log(`\n--- Creating ${DB_COLLECTION_COUNTERS} collection in "${DB_NAME}" for "${DB_COLLECTION}" collection" ---\n`);
     await db
       .collection(DB_COLLECTION_COUNTERS)
-      .insertOne({ _id: "issues", current: count });
+      .insertOne({ _id: 'issues', current: count });
 
     const indexes = await db.collection(DB_COLLECTION).indexes();
-    console.log(`Generated indexes:\n`);
+    console.log('Generated indexes:\n');
     indexes.forEach((index) => {
       console.log(index);
     });
@@ -98,7 +90,7 @@ async function initializeDB() {
     console.log(err);
   } finally {
     client.close();
-    console.log("\n--- Connection closed ---\n");
+    console.log('\n--- Connection closed ---\n');
   }
 }
 
--- api/server.js
+++ api/server.js
@@ -1,38 +1,39 @@
-const express = require("express");
-const { ApolloServer, UserInputError } = require("apollo-server-express");
-const fs = require("fs");
-const { GraphQLScalarType } = require("graphql");
-const { Kind } = require("graphql/language");
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const { ApolloServer, UserInputError } = require('apollo-server-express');
+const fs = require('fs');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
+const { MongoClient } = require('mongodb');
 const path = require('path');
 
-
-let aboutMessage = "Issue Tracker API v1.0";
+let aboutMessage = 'Issue Tracker API v1.0';
 
 const envPath = path.resolve(__dirname, '../.env');
-require("dotenv").config({ path: envPath });
+require('dotenv').config({ path: envPath });
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_USER_PASSWD;
-const DB_NAME = process.env.DB_NAME;
-const DB_COLLECTION = process.env.DB_COLLECTION;
-const DB_CLUSTER = process.env.DB_CLUSTER;
-const DB_COLLECTION_COUNTERS = process.env.DB_COLLECTION_COUNTERS;
-const SERVER_PORT_API = process.env.SERVER_PORT_API || 3000;
+const {
+  DB_USER,
+  DB_USER_PASSWORD,
+  DB_NAME,
+  DB_COLLECTION,
+  DB_CLUSTER,
+  DB_COLLECTION_COUNTERS,
+  SERVER_PORT_API,
+} = process.env;
 
-url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/`;
+const url = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_CLUSTER}/`;
 let db;
 
 async function connectToDb() {
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
-  console.log("Connected to MongoDB at", DB_CLUSTER);
+  console.log('Connected to MongoDB at', DB_CLUSTER);
   db = client.db(DB_NAME);
 }
 
 const GraphQLDate = new GraphQLScalarType({
-  name: "GraphQLDate",
-  description: "A Date() type in GraphQL as a scalar",
+  name: 'GraphQLDate',
+  description: 'A Date() type in GraphQL as a scalar',
 
   // Convert a Date to a String.
   serialize(value) {
@@ -41,51 +42,66 @@ const GraphQLDate = new GraphQLScalarType({
   parseValue(value) {
     // Catch invalid date strings
     const dateValue = new Date(value);
-    return isNaN(dateValue) ? undefined : dateValue;
+    return Number.isNaN(dateValue.getTime()) ? undefined : dateValue;
   },
   parseLiteral(ast) {
-    if (ast.kind == Kind.STRING) {
+    if (ast.kind === Kind.STRING) {
       const value = new Date(ast.value);
-      return isNaN(value) ? undefined : value;
+      return Number.isNaN(value.getTime()) ? undefined : value;
     }
+    return undefined;
   },
 });
 
-const resolvers = {
-  Query: {
-    about: () => aboutMessage,
-    issueList,
-  },
-  Mutation: {
-    setAboutMessage,
-    issueAdd,
-  },
-  GraphQLDate,
-};
+function setAboutMessage(_, { message }) {
+  aboutMessage = message;
+  return aboutMessage;
+}
+
+function issueValidate(issue) {
+  // Holds error messages of failed validations
+  const errors = [];
+  // Add minimum length for the issue title. If fails, a message is pushed into de errors array
+  if (issue.title.length < 3) {
+    errors.push('Field "title" must be a least 3 characters long.');
+  }
+  // Add a conditional mandatory validation that checks
+  // for the owner being required when the status is set to Assigned
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is requierd when status is "Assigned"');
+  }
+  // Use the UserInputError class to generate user errors.
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors });
+  }
+}
+
+async function getNextSequence(name) {
+  const result = await db.collection(DB_COLLECTION_COUNTERS).findOneAndUpdate(
+    { _id: name },
+    { $inc: { current: 1 } },
+    { returnOriginal: false },
+  );
+  return result.value.current;
+}
 
 async function issueAdd(_, { issue }) {
+  const issueCopy = { ...issue };
+  issueValidate(issueCopy);
+  issueCopy.created = new Date();
 
-  issueValidate(issue);
-  issue.created = new Date();
-
-  issue.id = await getNextSequence('issues');
-  
-  const result = await db.collection(DB_COLLECTION).insertOne(issue);
+  issueCopy.id = await getNextSequence('issues');
+  const result = await db.collection(DB_COLLECTION).insertOne(issueCopy);
   const savedIssue = await db.collection(DB_COLLECTION)
-    .findOne({_id: result.insertedId});
-  
+    .findOne({ _id: result.insertedId });
   return savedIssue;
 }
 
-function setAboutMessage(_, { message }) {
-  return (aboutMessage = message);
-}
-
 async function issueList() {
-  //const issues = await db.collection(DB_COLLECTION).find({}).toArray();
-  //return issues;
-  //If there are thousands of documents
-
+  const issues = await db.collection(DB_COLLECTION).find({}).toArray();
+  return issues;
+  // If there are thousands of documents
+  /*
   const batchSize = 1000; // Fetch 1000 documents at a time
   const cursor = db.collection(DB_COLLECTION).find({});
 
@@ -107,60 +123,47 @@ async function issueList() {
   }
 
   return issues;
+  */
 }
 
-function issueValidate(issue) {
-  // Holds error messages of failed validations
-  const errors = [];
-  // Add minimum length for the issue title. If fails, a message is pushed into de errors array
-  if (issue.title.length < 3) {
-    errors.push('Field "title" must be a least 3 characters long.');
-  }
-  // Add a conditional mandatory validation that checks for the owner being required when the status is set to Assigned
-  if (issue.status == "Assigned" && !issue.owner) {
-    errors.push('Field "owner" is requierd when status is "Assigned"');
-  }
-  // Use the UserInputError class to generate user errors.
-  if (errors.length > 0) {
-    throw new UserInputError("Invalid input(s)", { errors });
-  }
-}
-
-async function getNextSequence(name) {
-  const result = await db.collection(DB_COLLECTION_COUNTERS).findOneAndUpdate(
-    {_id: name},
-    {$inc: {current: 1}},
-    {returnOriginal: false},
-  );
-  return result.value.current;
-}
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+    issueList,
+  },
+  Mutation: {
+    setAboutMessage,
+    issueAdd,
+  },
+  GraphQLDate,
+};
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync("schema.graphql", "utf-8"),
+  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
   resolvers,
 
   formatError: (error) => {
     console.log(error);
-    //const { extensions: { errors }, ...rest } = error;
-    //return { ...rest, extensions: { errors } };
+    // const { extensions: { errors }, ...rest } = error;
+    // return { ...rest, extensions: { errors } };
     return error;
   },
 });
 
 const app = express();
 
-const enableCors = (process.env.ENABLE_CORS || 'true') == 'true';
+const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
 console.log('CORS setting:', enableCors);
 
-server.applyMiddleware({ app, path: "/graphql",cors: enableCors });
+server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 
-(async function () {
+(async function start() {
   try {
     await connectToDb();
-    app.listen(SERVER_PORT_API, function () {
+    app.listen(SERVER_PORT_API, () => {
       console.log(`API server started on port ${SERVER_PORT_API}`);
     });
   } catch (err) {
-    console.log("Error: ", err);
+    console.log('Error: ', err);
   }
-})();
+}());
