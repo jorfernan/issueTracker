e9751b757e0888d69e6a66264a7f36cc2c7b87a7
jorfernan <jorfernan1995@gmail.com>
2023-06-19 18:43:34 +0200

    Fron-End Modules and Webpack section completed succesfully

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,46 +2,13 @@
 /* globals React ReactDOM */
 /* eslint "react/jsx-no-undef": "off" */
 /* eslint "react/no-multi-comp": "off" */
-/* eslint-disable no-alert */
+
+import graphQLFetch from './graphQLFetch.js';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
   render() {
     return <div>This is a placeholder for the issue filter</div>;
-  }
-}
-
-const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
-function jsonDateReviver(key, value) {
-  if (dateRegex.test(value)) return new Date(value);
-  return value;
-}
-
-async function graphQLFetch(query, variables = {}) {
-  try {
-    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query, variables }),
-    });
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDateReviver);
-
-    if (result.errors) {
-      const error = result.errors[0];
-
-      if (error.extensions.code === 'BAD_USER_INPUT') {
-        const details = error.extensions.exception.errors.join('\n ');
-
-        alert(`${error.message}:\n ${details}`);
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`);
-      }
-    }
-    return result.data;
-  } catch (e) {
-    alert(`Error in sending data to the server ${e.message}`);
-    return undefined;
   }
 }
 
