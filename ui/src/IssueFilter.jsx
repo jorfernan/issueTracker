116e4040304d32a79306547dd2a731458b258f23
jorfernan <jorfernan1995@gmail.com>
2023-06-19 19:23:11 +0200

    ESLINT styles added to the new filees

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -2,7 +2,7 @@
 /* eslint "react/prefer-stateless-function": "off" */
 
 export default class IssueFilter extends React.Component {
-    render() {
-      return <div>This is a placeholder for the issue filter</div>;
-    }
+  render() {
+    return <div>This is a placeholder for the issue filter</div>;
   }
+}
--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -19,30 +19,28 @@ function IssueRow({ issue }) {
       <td>{effort}</td>
       <td>{due ? due.toDateString() : ''}</td>
       <td>{title}</td>
-      </tr>
+    </tr>
   );
 }
 
 export default function IssueTable({ issues }) {
-    const issueRows = issues.map(issue => (
-      <IssueRow key={issue.id} issue={issue} />
-    ));
-    return (
-      <table className="bordered-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Status</th>
-            <th>Owner</th>
-            <th>Created</th>
-            <th>Effort</th>
-            <th>Due Date</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>{issueRows}</tbody>
-      </table>
-    );
-  }
-
-  
+  const issueRows = issues.map(issue => (
+    <IssueRow key={issue.id} issue={issue} />
+  ));
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Due Date</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>{issueRows}</tbody>
+    </table>
+  );
+}
