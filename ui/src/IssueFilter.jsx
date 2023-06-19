129c4b7dd13af5e16e7c396c8c3cf56458a66468
jorfernan <jorfernan1995@gmail.com>
2023-06-19 19:11:31 +0200

    App.jsx divided into smaller files, each one with one component

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,8 @@
+/* globals React */
+/* eslint "react/prefer-stateless-function": "off" */
+
+export default class IssueFilter extends React.Component {
+    render() {
+      return <div>This is a placeholder for the issue filter</div>;
+    }
+  }
---
+++ ui/src/IssueTable.jsx
@@ -0,0 +1,48 @@
+/* globals React */
+
+function IssueRow({ issue }) {
+  const {
+    id,
+    status,
+    owner,
+    created,
+    effort,
+    due,
+    title,
+  } = issue;
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{status}</td>
+      <td>{owner}</td>
+      <td>{created.toDateString()}</td>
+      <td>{effort}</td>
+      <td>{due ? due.toDateString() : ''}</td>
+      <td>{title}</td>
+      </tr>
+  );
+}
+
+export default function IssueTable({ issues }) {
+    const issueRows = issues.map(issue => (
+      <IssueRow key={issue.id} issue={issue} />
+    ));
+    return (
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+
+  
