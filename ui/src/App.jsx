6ea5d2380fbaee7b9467fdd04c0e805168c8abfc
jorfernan <jorfernan1995@gmail.com>
2023-06-20 11:10:45 +0200

    Simple router using React Router implemented

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,10 +2,14 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import Page from './Page.jsx';
 
-import IssueList from './IssueList.jsx';
-
-const element = <IssueList />;
+const element = (
+    <Router>
+        <Page />
+    </Router>
+);
 
 ReactDOM.render(element, document.getElementById('root'));
 
